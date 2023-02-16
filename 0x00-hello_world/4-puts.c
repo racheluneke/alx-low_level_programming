273768aa1f34a554380b53025932bdff3832678d
racheluneke <rackelchi07@gmail.com>
2023-02-16 14:49:50 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - A programme that prints a line with put function
- *   * Return: 0 (success)
- *    */
+ * main - A programme that prints a line with put function
+ *  Return: 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
-}
+	return (0):
+}	
