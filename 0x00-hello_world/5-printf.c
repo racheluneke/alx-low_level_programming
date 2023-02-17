95e3b9fb9e978136cc6caf5ca9bce46b7d6ff911
racheluneke <rackelchi07@gmail.com>
2023-02-17 14:14:22 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-/**  main - Entry point
-
-  
-
- *  *   Return: Always 0 (Success)
- *     */
+/**
+ * main - Entry point
+ *  
+ * Return: 0 
+ */
 int main(void)
-{ 
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-			printf("Size of an int: %d byte(s)\n", sizeof(int));
-				printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %d byte(s)\n", sizeof(float));
-							return (0);
+{
+	printf("with proper grammer,but the outcome is a piece of art\n");
+	return (0);
+
+
 }
-xet-yqnx-qr
