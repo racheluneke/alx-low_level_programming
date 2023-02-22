7920d9a6a42b598c721b6da830c86057fad7a632
racheluneke <rackelchi07@gmail.com>
2023-02-21 19:14:23 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 /**
  * print_alphabet - prints lowercase alphabets
  *
- * Rturn: Always 0
+ * Return: Always 0
  */
 void  print_alphabet(void)
 {
 	char low_alpha;
 
-	for (low_alpha = 'a'; low_alpha <='z'; low_alpha++)
+	for (low_alpha = 'a'; low_alpha <= 'z'; low_alpha++)
 		_putchar(low_alpha);
-	_putchar('/n');
+	_putchar('\n');
 }
