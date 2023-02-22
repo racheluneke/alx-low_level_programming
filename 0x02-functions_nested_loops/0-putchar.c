38b59082e4566f9b3dc6854d4b75b346621b95e1
racheluneke <rackelchi07@gmail.com>
2023-02-21 17:48:30 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,19 @@
-#!/bin/bash
-_putchar
+#include "main.h"
+/**
+ * main - Prints string to screen
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
