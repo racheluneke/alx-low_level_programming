c9f7c44190d3916986c5e1595ba4479d9916820f
racheluneke <rackelchi07@gmail.com>
2023-02-23 00:45:33 -0800

    hundred times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,8 +30,7 @@ void print_times_table(int n)
 				else if (op <= 99)
 				{
 					_putchar(32);
-					_
-_putchar((op / 10) + 48);
+					_putchar((op / 10) + 48);
 					_putchar((op % 10) + 48);
 				}
 				else
