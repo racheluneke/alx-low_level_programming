21181025613fe0cf8e09618a0688c85a9818042f
racheluneke <rackelchi07@gmail.com>
2023-03-01 13:27:19 -0800

    o strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _strcat - coctenates two strings
+ * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
  * Return: void
  */
-char *_strcat(char "dest, char "src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
-	i=0;
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j =0;
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -26,4 +26,3 @@ char *_strcat(char "dest, char "src)
 
 	dest[i] - '\0';
 }	return (dest);
-
