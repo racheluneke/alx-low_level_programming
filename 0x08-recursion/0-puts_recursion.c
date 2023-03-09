412d317e484e45fe7549381083801c3c41efbadd
racheluneke <rackelchi07@gmail.com>
2023-03-08 18:25:43 -0800

    0-put

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - checks the length of string
- * @s: string variable
- *
- * Return: returns length
+ * _puts_recursion - prints a string followed by new line
+ * @s: character variable
  */
 
-int _strlen_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-		return (1 + _strlen_recursion(s + 1));
-
-	return (0);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
