815aeebf7d7241595b4fc7ca99db8c2a4fd922a9
racheluneke <rackelchi07@gmail.com>
2023-03-09 19:56:23 -0800

    palingrone

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,43 +2,43 @@
 
 /**
  * _strlen_recursion - returns the length of a string.
- * @p: string
+ * @s: string
  * Return: the length of a string.
  */
-int _strlen_recursion(char *p)
+int _strlen_recursion(char *s)
 {
-	if (*p == '\0')
+	if (*s == '\0')
 		return (0);
 	else
-		return (1 + _strlen_recursion(p + 1));
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
  * comparator - compares each character of the string.
- * @p: string
+ * @s: string
  * @n1: smallest iterator.
  * @n2: biggest iterator.
  * Return: .
  */
-int comparator(char *p, int n1, int n2)
+int comparator(char *s, int n1, int n2)
 {
-	if (*(p + n1) == *(p + n2))
+	if (*(s + n1) == *(s + n2))
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
-		return (0 + comparator(p, n1 + 1, n2 - 1));
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
 
 /**
  * is_palindrome - detects if a string is a palindrome.
- * @p: string.
+ * @s: string.
  * Return: 1 if s is a palindrome, 0 if not.
  */
-int is_palindrome(char *p)
+int is_palindrome(char *s)
 {
-	if (*p == '\0')
+	if (*s == '\0')
 		return (1);
-	return (comparator(p, 0, _strlen_recursion(p) - 1));
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
