11b75fc9dc5e29894bcb95343fedb568c58092e7
racheluneke <rackelchi07@gmail.com>
2023-03-09 20:33:25 -0800

    recursion parameters

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,15 +6,15 @@
  * @s2: string 2. It can contains a * as a special character.
  * Return: 1 if are identical, 0 if not.
  */
-int wildcmp(char *n1, char *n2)
+int wildcmp(char *s1, char *s2)
 {
-	if (*n2 == '*' && *(n2 + 1) != '\0' && *n1 == '\0')
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		return (0);
-	if (*n1 == '\0' && *n2 == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*n1 == *n2)
-		return (wildcmp(n1 + 1, n2 + 1));
-	if (*n2 == '*')
-		return (wildcmp(n1, n2 + 1) || wildcmp(n1 + 1, n2));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
 }
