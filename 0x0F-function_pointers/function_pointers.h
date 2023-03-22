215c5d345ee1a7bfa6cd893a0b75a20214ab8eba
racheluneke <rackelchi07@gmail.com>
2023-03-22 13:29:43 -0700

    my function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifndef function_pointers.h
-#define function_pointers.h
+#ifndef function_pointers_h
+#define function_pointers_h
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
