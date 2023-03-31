6eefcb072acd0b81ff6c7607143ae7b6157afb95
racheluneke <rackelchi07@gmail.com>
2023-03-31 00:15:31 -0700

    my list.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,14 @@
 #ifndef list_h
 #define list_h
 
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
