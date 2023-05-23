285b3d65f8ddf46db546f1c5a5e4b39df0fdb369
racheluneke <rackelchi07@gmail.com>
2023-05-22 21:25:45 -0700

    scripting more details

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/racheluneke/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/racheluneke/
+alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
